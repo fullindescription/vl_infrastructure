7d69224772890f6f34e6a74af8cdf5fd0348555e
Marsvest <alajh.akbar@gmail.com>
2024-10-23 21:03:00 +1000

    service control

---
+++ service_control.sh
@@ -0,0 +1,26 @@
+start() {
+    docker compose -f vl_infrastructure/docker-compose.yml up --pull always -d
+}
+
+stop() {
+    docker compose -f vl_infrastructure/docker-compose.yml down
+    sleep 10
+}
+
+sync() {
+    cd vl_infrastructure
+    git pull
+}
+
+case "$1" in
+    (start)
+        start
+        ;;
+    (stop)
+        stop
+        ;;
+    (*)
+        echo "Использование: $0 {start|stop}"
+        exit 1
+        ;;
+esac
